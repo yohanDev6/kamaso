f4504d3f6da31e81aadab3c43f020fe2d4ceaef0
yohanDev6 <yohan.dev6@gmail.com>
2024-08-02 19:24:13 -0300

    Camisas pedidas estilizadas

--- src/assets/pages/shirtsOrdered/ShirtsOrdered.js
+++ src/assets/pages/shirtsOrdered/ShirtsOrdered.js
@@ -1,9 +1,83 @@
+import { IoTrashOutline } from 'react-icons/io5';
+import shirtImg from '../../imgs/post/images.jpg';
+import './ShirtsOrderedStyle.css';
 
-
-export default function ShirtsOrdered(){
+export default function ShirtsOrdered() {
     return (
-        <aside>
-            Shirts Ordereds
+        <aside className='shirts-ordered'>
+            <h3>Camisas pedidas</h3>
+            <ShirtOrdered 
+            img={shirtImg}
+            title="INFO4M"
+            price={49.99}
+            description={{
+                seller: "INFO4M",
+                style: "M Adulto, Preta, 41, João",
+                payment: "PIX 1X"
+            }}
+            paymentStatus="PAGO"
+            shirtStatus="ENTREGUE"
+            />
+            <ShirtOrdered
+            img={shirtImg} 
+            title="INFO4M"
+            price={49.99}
+            description={{
+                seller: "INFO4M",
+                style: "M Adulto, Preta, 41, João",
+                payment: "PIX 1X"
+            }}
+            paymentStatus="PAGO"
+            shirtStatus="ENTREGUE"
+            />
         </aside>
+    );
+}
+
+function ShirtOrdered({ img, title, price, description, paymentStatus, shirtStatus }) {
+    return (
+        <div className="order">
+            <div className="shirt-img">
+                <img src={img} alt="Imagem da camisa"/>
+            </div>
+            <div className="about">
+                <div className="text">
+                    <div className="top-text">
+                        <p className="title">{title}</p>
+                        <p className="price">R$ {price}</p>
+                    </div>
+                    <div className="bottom-text">
+                        <h4>Características</h4>
+                        <Description content={description}/>
+                    </div>
+                </div>
+                <div className="so-actions">
+                    <button className="trash">
+                        <IoTrashOutline className="icon"/>
+                    </button>
+                    <div className="tags">
+                        <div className="paid shape">
+                            <p>{paymentStatus}</p>
+                        </div>
+                        <div className="delivered shape">
+                            <p>{shirtStatus}</p>
+                        </div>
+                        <button className='contact'>
+                            <p>CONTATO</p>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Description({content}){
+    return (
+        <div className="description">
+            <p>Camisa vendida por {content.seller}</p>
+            <p>{content.style}</p>
+            <p>{content.payment}</p>
+        </div>
     );
 }
